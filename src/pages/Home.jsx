02566c259a1342886e1ee2e969ad0dc11aa6970a
Fabithelast25 <fabian.robles06@inacapmail.cl>
2025-05-18 16:45:02 -0400

    Update de Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,10 @@
-import CardProducto from '../components/CardProducto';
-import { getUserData } from "../services/userService";
-import { useAuth } from "../context/AuthContext";
-
-useEffect(() => {
-  const fetch = async () => {
-  const datos = await getUserData(user.uid);
-  setUserData(datos);
-  };
-  if (user) fetch();
-  }, [user]);  
-  
-function Home() {
-  return (
-    <div className="container mt-4">
-      <h1>Productos Disponibles</h1>
-      <CardProducto nombre="Pan Integral" precio="$500" />
-      <CardProducto nombre="Pan Integral" precio="$500" />
-      <CardProducto nombre="Pan Integral" precio="$300" />
-      <CardProducto nombre="Pan 22222222222" precio="$500" />
-      <CardProducto nombre="Pan Integral" precio="$500" />
-      <CardProducto nombre="Pan Integral" precio="$500" />
-
-    </div>
-  );
-}
-
-export default Home;
+import CerrarSesion from "../components/CerrarSesion"; 
+ 
+function Home() { 
+    return ( 
+        <div> 
+            <h2>Bienvenido a EcoFood</h2> 
+            <CerrarSesion /> 
+        </div> 
+    ); 
+} 
