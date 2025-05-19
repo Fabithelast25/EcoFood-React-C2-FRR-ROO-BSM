48aec4d389e5ecd86056ea35f9e887f2810eb41a
rafa <RAFAEL.ORREGO02@INACAPMAIL.CL>
2025-05-19 03:02:30 -0400

    ARREGLO GENERAL DE BUGS E INCONSISTENCIA DE RUTAS
    (la pagina se queda en blanco se debe encontrar la solucion a esto)

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,13 +5,13 @@ import Home from "../pages/Home";
 import ProtectedRoute from "./ProtectedRoute";
 export default function AppRouter() {
 return (
-<Routes>
-<Route path="/login" element={<Login />} />
-<Route path="/" element={<Login />} />
-<Route path="/registro" element={<Register />} />
-<Route path="/home" element={
-<ProtectedRoute>
-<Home />
+    <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/home" element={
+            <ProtectedRoute>
+    <Home />
 </ProtectedRoute>
 } />
 </Routes>
