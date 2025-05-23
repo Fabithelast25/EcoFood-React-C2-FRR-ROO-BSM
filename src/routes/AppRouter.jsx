a925cc1ef9b9f5bd79aed19995fae59d8488e21d
Fabithelast25 <fabian.robles06@inacapmail.cl>
2025-05-23 11:46:12 -0400

    ARREGLO APP ROUTER

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,12 +3,13 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Home from "../pages/Home"
 import ProtectedRoute from './ProtectedRoute'
+import RecuperarContraseña from "../pages/RecuperarContraseña";
 
 export default function AppRouter() {
 return (
     <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<> hola como estampos</>} />
+        <Route path="/" element={<Login />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/recuperar-password" element={<RecuperarContraseña />} />
         <Route path="/home" element={ 
