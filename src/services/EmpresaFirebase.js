a047e531f72fb01cb5ef815762eb304d888db1c0
Fabithelast25 <fabian.robles06@inacapmail.cl>
2025-06-03 18:43:19 -0400

    Update EmpresaFirebase.js

--- src/services/EmpresaFirebase.js
+++ src/services/EmpresaFirebase.js
@@ -1,42 +1,50 @@
-import { db } from "./firebase";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
+import { secondaryAuth, db } from "./firebase";
+import { setDoc, doc, getDocs, collection, updateDoc, deleteDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 
 export const getEmpresas = async () => {
-  const q = query(
-    collection(db, "empresas"),
-    where("tipo", "==", "empresa")
-  );
-  const snapshot = await getDocs(q);
-  return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  const snapshot = await getDocs(collection(db, "usuarios"));
+  return snapshot.docs
+    .map(doc => ({ id: doc.id, ...doc.data() }))
+    .filter(user => user.tipo === "empresa");
 };
 
-export const addEmpresa = async ({ nombre, rut, direccion, comuna, email, telefono }) => {
-  return await addDoc(collection(db, "empresas"), {
-    nombre,
-    rut,
-    direccion,
-    comuna,
-    email,
-    telefono,
-    tipo: "empresa",
-  });
+export const addEmpresa = async (datos) => {
+   try {
+    const cred = await createUserWithEmailAndPassword(
+      secondaryAuth,
+      datos.email,
+      datos.password
+    );
+    await sendEmailVerification(cred.user);
+    await setDoc(doc(db, "usuarios", cred.user.uid), {
+      nombre: datos.nombre || "",
+      rut: datos.rut || "",
+      comuna: datos.comuna || "",
+      direccion: datos.direccion || "",
+      tipo: "empresa",
+      email: datos.email || "",
+      telefono: datos.telefono || "",
+      
+    });
+    await secondaryAuth.signOut();
+    return cred;
+  } catch (error) {
+    console.error("Error registrando empresa:", error);
+    throw error;
+  }
 };
+
 
 export const updateEmpresa = async (id, empresaData) => {
-  const ref = doc(db, "empresas", id);
+  const ref = doc(db, "usuarios", id);
   return await updateDoc(ref, empresaData);
 };
 
 export const deleteEmpresa = async (id) => {
-  const ref = doc(db, "empresas", id);
+  const ref = doc(db, "usuarios", id);
   return await deleteDoc(ref);
 };
+
+
+ 
